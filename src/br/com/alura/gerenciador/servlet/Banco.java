66eab7451f44a489fbe507c67f6b0a75f4174074
FlaviaVilar <flaviavilar.github.com>
2018-12-11 14:40:12 -0200

    CRUD - deletar

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,15 +1,19 @@
 package br.com.alura.gerenciador.servlet;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
-
+	private static Integer chaveSequencial =1;
+	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Google");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
@@ -18,6 +22,7 @@ public class Banco {
 	
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 		
 		
@@ -25,6 +30,20 @@ public class Banco {
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void remove(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		
+		}
+		
 	}
 
 }
